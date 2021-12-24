1bcdc6fcb8e474309d57981d424fc8bf75e105d6
SHIVAMSAGAR05 <shivam.sagar.shivam@gmail.com>
2021-12-25 01:20:30 +0530

    git solutions

---
+++ input and Output.js
@@ -0,0 +1,10 @@
+// Please take input and print output to standard input/output (stdin/stdout)
+// DO NOT USE ARGUMENTS FOR INPUTS
+// YOUR CODE GOES HERE
+
+
+function sayHelloTo(name){    
+    return ("Hello" + " "+ name)
+}
+name=readLine();
+console.log(sayHelloTo(name))
